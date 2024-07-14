839ef9ec80ed4b14f2708d72bb6dfd6f85adfc55
Muhammad Milon <137856681+muhammadMilon@users.noreply.github.com>
2024-07-14 12:27:08 +0600

    Create Prefix_XOR.cpp

---
+++ Basics/Prefix_XOR.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+const int N = 2e5 + 5;
+const int mod = 1e9 + 7;
+ll Prefix_XOR[N], arr[N];
+
+int32_t main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    ll n, m; 
+    cin >> n >> m;
+
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 1; i <= n; i++) {
+        Prefix_XOR[i] = Prefix_XOR[i-1] ^ arr[i];
+    }
+
+    while (m--) {
+        ll a, b; 
+        cin >> a >> b;
+        cout << (Prefix_XOR[b] ^ Prefix_XOR[a-1]) << '\n';
+    }
+
+    return 0;
+}
+//https://cses.fi/problemset/task/1650
