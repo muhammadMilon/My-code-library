004db7817c1fb1e9f74a95c675c59a38f40aeaac
Muhammad Milon <137856681+muhammadMilon@users.noreply.github.com>
2025-06-28 22:41:52 +0600

    Update Sieve of Eratosthenes.cpp

--- Number Theory/Sieve of Eratosthenes.cpp
+++ Number Theory/Sieve of Eratosthenes.cpp
@@ -1,28 +1,36 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define int long long
 using namespace std;
 
-const int N = 1e7 + 9;
-bitset<N> f;
+const int N = 1e5 + 5;
+vector<int> prime;
+bool vis[N];
+
+void sieve() {
+    for(int i = 2; i * i < N; i++) {
+        if(!vis[i]) {
+            for(int j = i * i; j < N; j += i) {
+                vis[j] = true;
+            }
+        }
+    }
+
+    for(int i = 2; i < N; i++) {
+        if(!vis[i]) {
+            prime.push_back(i);
+        }
+    }
+}
+
 int32_t main() {
-  ios_base::sync_with_stdio(0);
-  cin.tie(0);
-  int n = N - 9;
-  vector<int> primes;
-  f[1] = true;
-  for (int i = 2; i * i <= n; i++) {
-    if (!f[i]) {
-      for (int j = i * i; j <= n; j += i) {
-        f[j] = true;
-      }
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    sieve();
+    for(int i = 0; i < 10; i++) {
+        cout << prime[i] << ' ';
     }
-  }
-  for (int i = 2; i <= n; i++) {
-    if (!f[i]) {
-      primes.push_back(i);
-    }
-  }
-  for(int i = 0; i < 5; i++){
-      cout << primes[i] << ' ';
-  }
-  return 0;
+    cout << '\n';
+
+    return 0;
 }
